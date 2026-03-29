34f82d5aaf874dfa36c082042b5d8aac4112eac2
Dhanuja <vdhanujakumar@gmail.com>
2026-03-29 16:17:04 +0530

    Fix formatting in hash.js

--- backend/hash.js
+++ backend/hash.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 
-const password = "123456"; // your password
+const password = "123456"; 
 
 bcrypt.hash(password, 10).then(hash => {
   console.log("Hashed password:", hash);
